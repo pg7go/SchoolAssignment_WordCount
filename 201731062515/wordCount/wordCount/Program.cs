cfb608fc4dc805bd33114059572f3cc70ab42fbd
pg7go <13982719614@163.com>
2019-03-30 11:37:14 +0800

    增加新功能

--- 201731062515/wordCount/wordCount/Program.cs
+++ 201731062515/wordCount/wordCount/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -10,17 +11,47 @@ namespace wordCount
     {
         static void Main(string[] args)
         {
-            if(args.Length==0)
+            string path=null;
+            string output=null;
+            int split = 1;
+            int count = 10;
+
+            for (int i = 0; i < args.Length; i += 2)
             {
-                Console.WriteLine("请加入参数，eg:wordCount.exe input.txt");
+                if (!(args.Length > i + 1))
+                    break;
+                if (args[i] == "-i")
+                    path = args[i + 1];
+                else if (args[i] == "-m")
+                {
+                    if (!int.TryParse(args[i + 1], out split))
+                        split = 1;
+                }
+                else if (args[i] == "-n")
+                {
+                    if (!int.TryParse(args[i + 1], out count))
+                        count = 10;
+                }
+                else if (args[i] == "-o")
+                {
+                    output = args[i + 1];
+                }
+            }
+
+
+
+
+            if (string.IsNullOrEmpty(path)|| string.IsNullOrEmpty(output))
+            {
+                Console.WriteLine("请加入输入与输出的参数");
                 return;
             }
 
 
             string text;
-            if(OpenFile(args[0],out text))
+            if(OpenFile(path, out text))
             {
-                Analyze(text);
+                Analyze(text,split,count,output);
             }
             else
             {
@@ -29,10 +60,15 @@ namespace wordCount
         }
 
 
-
-        public static void Analyze(string text)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text">用于分析的文本</param>
+        /// <param name="split">分割数目</param>
+        /// <param name="count">输出单词数量</param>
+        public static void Analyze(string text,int split=1,int count=10,string output="output.txt")
         {
-            var words = CountWord(text);
+            var words = CountWord(text,split);
             var list = words.ToList();
             list.Sort((x, y) =>
             {
@@ -41,13 +77,29 @@ namespace wordCount
                 return -x.Value.CompareTo(y.Value);
             });
 
-            Console.WriteLine("characters: {0}", CountChar(text));
-            Console.WriteLine("words: {0}", words.Count);
-            Console.WriteLine("lines: {0}", CountLine(text));
-            for (int i = 0; i < 10; i++)
+            StreamWriter sw = new StreamWriter(output);
+            try
             {
-                if(list.Count>=i+1)
-                    Console.WriteLine("{0}: {1}", list[i].Key,list[i].Value);
+                Console.SetOut(sw);
+
+                Console.WriteLine("characters: {0}", CountChar(text));
+                Console.WriteLine("words: {0}", words.Count);
+                Console.WriteLine("lines: {0}", CountLine(text));
+                for (int i = 0; i < count; i++)
+                {
+                    if (list.Count >= i + 1)
+                        Console.WriteLine("{0}: {1}", list[i].Key, list[i].Value);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输出文件错误！");
+            }
+            finally
+            {
+                sw.Flush();
+                sw.Close();
             }
 
         }
@@ -55,11 +107,13 @@ namespace wordCount
 
 
 
-        public static Dictionary<string,int> CountWord(string text)
+        public static Dictionary<string,int> CountWord(string text,int splite=1)
         {
             Dictionary<string, int> dict = new Dictionary<string, int>();
 
+            int nowsplite = 0;
             string starter = "";
+            bool isChecked= false;
             foreach (var item in text)
             {
                 if (char.IsNumber(item))
@@ -67,6 +121,7 @@ namespace wordCount
                     if (starter.Length >= 4)
                     {
                         starter += item;
+                        isChecked = true;
                         continue;
                     }
 
@@ -75,10 +130,22 @@ namespace wordCount
                 {
                     var letter = char.ToLower(item);
                     starter += letter;
+                    isChecked = true;
                     continue;
                 }
                 else if (starter.Length == 0)
                     continue;
+
+                if(isChecked)
+                { isChecked = false; nowsplite++; }
+                    
+                if (nowsplite <= splite)
+                {
+                    starter += item;
+                    continue;
+                }
+                    
+                nowsplite = 0;
 
                 if (dict.ContainsKey(starter))
                     dict[starter]++;
